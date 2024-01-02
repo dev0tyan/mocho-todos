a8ed8cac9d76556dbb9991bc20af51b1bca91e02
dev0tyan <diaz.christian.cpe@gmail.com>
2024-01-02 15:32:59 +0800

    Small changes in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from "react";
 
 export default function App() {
   let today = new Date();
   const getDate = today.toDateString();
   const [todos, setTodos] = useState([]);
+  const [content, setContent] = useState('');
 
   useEffect(() => {
     async function getTodos() {
@@ -22,20 +23,29 @@ export default function App() {
     <main className="container">
       <h3 className="date">{getDate}</h3>
       <div className="todoCount">4 task</div>
-      <input type="text" placeholder="Add a new todo" id="inputBox" />
-      <div className="todos">
-        {todos.length > 0 &&
-          todos.map((todo) => (
-            <div key={todo._id} className="todo">
-              <div>
-                <button className="todo__status">
-                  {todo.status ? <FontAwesomeIcon icon={faCircleCheck} style={{color: "#62dfa9",}} /> : <FontAwesomeIcon icon={faCircle} style={{color: "#a0a2a6",}} />}
-                </button>
+      <form className='form'>
+        <input 
+          className='form__input'
+          type="text"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Add a new todo" 
+          id="inputBox"
+        />
+        <div className="todos">
+          {todos.length > 0 &&
+            todos.map((todo) => (
+              <div key={todo._id} className="todo">
+                <div>
+                  <button className="todo__status">
+                    {todo.status ? <FontAwesomeIcon icon={faCircleCheck} style={{color: "#62dfa9",}} /> : <FontAwesomeIcon icon={faCircle} style={{color: "#a0a2a6",}} />}
+                  </button>
+                </div>
+                <p>{todo.todo}</p>
               </div>
-              <p>{todo.todo}</p>
-            </div>
-          ))}
-      </div>
+            ))}
+        </div>
+      </form>
     </main>
   );
 }
